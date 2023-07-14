89dbbf6a2ed8d7ede5cfb72b3bdb050167e3d881
Evans <ngugie649@gmail.com>
2023-07-14 12:36:34 +0300

    calendar

---
+++ src/pages/Calendar/Calendar.jsx
@@ -0,0 +1,15 @@
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
+import './calendar.scss'
+
+function Calendar() {
+    return (
+        <div className='Calendar'>
+            <ScheduleComponent style={{height: "100vh"}} currentView='Month' >
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda]} ></Inject>
+            </ScheduleComponent>
+            
+        </div>
+    )
+}
+
+export default Calendar;
